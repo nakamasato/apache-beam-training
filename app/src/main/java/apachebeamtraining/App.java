a8e581bc22fdfd1a58f6f21d1c1062103b7e5b76
Masato Naka <masatonaka1989@gmail.com>
2022-09-02 16:37:22 +0900

    chore: fix indent

--- app/src/main/java/apachebeamtraining/App.java
+++ app/src/main/java/apachebeamtraining/App.java
@@ -20,119 +20,117 @@ import org.slf4j.LoggerFactory;
 
 public class App {
 
-    private static Logger logger = LoggerFactory.getLogger(App.class);
+  private static Logger logger = LoggerFactory.getLogger(App.class);
 
-    /*
-     * Extract 5th field of the input text.
-     */
-    public static class ExtractAmountFromRowFn extends DoFn<String, String> {
-        @ProcessElement
-        public void process(ProcessContext c) {
+  /*
+   * Extract 5th field of the input text.
+   */
+  public static class ExtractAmountFromRowFn extends DoFn<String, String> {
+    @ProcessElement
+    public void process(ProcessContext c) {
 
-            try {
-                String row = c.element();
+      try {
+        String row = c.element();
 
-                String[] cells = row.split(",");
+        String[] cells = row.split(",");
 
-                c.output(cells[4]);
-            } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-                logger.error("Failed to process data", e);
-            }
-        }
+        c.output(cells[4]);
+      } catch (java.lang.ArrayIndexOutOfBoundsException e) {
+        logger.error("Failed to process data", e);
+      }
     }
+  }
 
-    /**
-     * KV<String, Iterable<Integer> -> String
-     */
-    static class ConvertKVToStringFn extends DoFn<KV<String, Iterable<Integer>>, String> {
-        @ProcessElement
-        public void processElement(ProcessContext c) {
+  /**
+   * KV<String, Iterable<Integer> -> String
+   */
+  static class ConvertKVToStringFn extends DoFn<KV<String, Iterable<Integer>>, String> {
+    @ProcessElement
+    public void processElement(ProcessContext c) {
 
-            c.output(String.valueOf(c.element()));
+      c.output(String.valueOf(c.element()));
 
-        }
     }
+  }
 
-    /*
-     * String -> KV<String, Integer>
-     */
-    static class ConvertStringIntoKVFn extends DoFn<String, KV<String, Integer>> {
-        @ProcessElement
-        public void processElement(ProcessContext c) {
-            String row = c.element();
-            String[] cells = row.split(",");
-            c.output(KV.of(cells[0], row.length()));
-        }
+  /*
+   * String -> KV<String, Integer>
+   */
+  static class ConvertStringIntoKVFn extends DoFn<String, KV<String, Integer>> {
+    @ProcessElement
+    public void processElement(ProcessContext c) {
+      String row = c.element();
+      String[] cells = row.split(",");
+      c.output(KV.of(cells[0], row.length()));
     }
+  }
 
-    /*
-     * String -> KV<String, CryptoCurrency>
-     */
-    static class ConvertTextIntoKVCryptoCurrencyFn
-            extends DoFn<String, KV<String, CryptoCurrency>> {
-        @ProcessElement
-        public void processElement(@Element String row,
-                OutputReceiver<KV<String, CryptoCurrency>> receiver) {
-            String[] cells = row.split(",");
-            try {
-                double amount = Double.parseDouble(cells[4]);
-                receiver.output(KV.of(cells[0], new CryptoCurrency(cells[0], amount)));
-            } catch (NumberFormatException ex) {
-                logger.error("failed to convert to CryptoCurrency", ex);
-            } catch (ArrayIndexOutOfBoundsException ex) {
-                logger.error("failed to convert to CryptoCurrency", ex);
-            }
-        }
+  /*
+   * String -> KV<String, CryptoCurrency>
+   */
+  static class ConvertTextIntoKVCryptoCurrencyFn extends DoFn<String, KV<String, CryptoCurrency>> {
+    @ProcessElement
+    public void processElement(@Element String row,
+        OutputReceiver<KV<String, CryptoCurrency>> receiver) {
+      String[] cells = row.split(",");
+      try {
+        double amount = Double.parseDouble(cells[4]);
+        receiver.output(KV.of(cells[0], new CryptoCurrency(cells[0], amount)));
+      } catch (NumberFormatException ex) {
+        logger.error("failed to convert to CryptoCurrency", ex);
+      } catch (ArrayIndexOutOfBoundsException ex) {
+        logger.error("failed to convert to CryptoCurrency", ex);
+      }
     }
+  }
 
-    /*
-     * Process item in a batch. Just count the number of item and return BatchResult as String.
-     */
-    static class ProcessBatch extends DoFn<KV<String, Iterable<CryptoCurrency>>, String> {
-        @ProcessElement
-        public void process(@Element KV<String, Iterable<CryptoCurrency>> batch,
-                OutputReceiver<String> receiver) {
-            int count = 0;
-            Iterator<CryptoCurrency> iter = batch.getValue().iterator();
-            while (iter.hasNext()) {
-                count++;
-                iter.next();
-            }
-            BatchResult batchResult =
-                    new BatchResult(batch.getKey(), count, new Date(System.currentTimeMillis()));
-            receiver.output(batchResult.toString());
-        }
+  /*
+   * Process item in a batch. Just count the number of item and return BatchResult as String.
+   */
+  static class ProcessBatch extends DoFn<KV<String, Iterable<CryptoCurrency>>, String> {
+    @ProcessElement
+    public void process(@Element KV<String, Iterable<CryptoCurrency>> batch,
+        OutputReceiver<String> receiver) {
+      int count = 0;
+      Iterator<CryptoCurrency> iter = batch.getValue().iterator();
+      while (iter.hasNext()) {
+        count++;
+        iter.next();
+      }
+      BatchResult batchResult =
+          new BatchResult(batch.getKey(), count, new Date(System.currentTimeMillis()));
+      receiver.output(batchResult.toString());
     }
+  }
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        PipelineOptions options = PipelineOptionsFactory.create();
+    PipelineOptions options = PipelineOptionsFactory.create();
 
-        Pipeline p = Pipeline.create(options);
+    Pipeline p = Pipeline.create(options);
 
-        // Input: read from file
-        PCollection<String> textData = p.apply(TextIO.read().from("input-record.txt"));
+    // Input: read from file
+    PCollection<String> textData = p.apply(TextIO.read().from("input-record.txt"));
 
-        // Process1: Group by the cryptocurrency name and write each line length to the file.
-        PCollection<KV<String, Integer>> mapped =
-                textData.apply(ParDo.of(new ConvertStringIntoKVFn()));
-        PCollection<KV<String, Iterable<Integer>>> groupByKey =
-                mapped.apply(GroupByKey.<String, Integer>create());
-        PCollection<String> count = groupByKey.apply(ParDo.of(new ConvertKVToStringFn()));
-        count.apply(TextIO.write().to("output-aggregated"));
+    // Process1: Group by the cryptocurrency name and write each line length to the file.
+    PCollection<KV<String, Integer>> mapped = textData.apply(ParDo.of(new ConvertStringIntoKVFn()));
+    PCollection<KV<String, Iterable<Integer>>> groupByKey =
+        mapped.apply(GroupByKey.<String, Integer>create());
+    PCollection<String> count = groupByKey.apply(ParDo.of(new ConvertKVToStringFn()));
+    count.apply(TextIO.write().to("output-aggregated"));
 
-        // Process2: Extract the amount of each line and write it to the file.
-        PCollection<String> bidData = textData.apply(ParDo.of(new ExtractAmountFromRowFn()));
-        bidData.apply(TextIO.write().to("output-map"));
+    // Process2: Extract the amount of each line and write it to the file.
+    PCollection<String> bidData = textData.apply(ParDo.of(new ExtractAmountFromRowFn()));
+    bidData.apply(TextIO.write().to("output-map"));
 
-        // Process3: GroupIntoBatches
-        PCollection<KV<String, CryptoCurrency>> cryptoKV =
-                textData.apply(ParDo.of(new ConvertTextIntoKVCryptoCurrencyFn()));
-        PCollection<KV<String, Iterable<CryptoCurrency>>> batchedCrypt =
-                cryptoKV.apply(GroupIntoBatches.<String, CryptoCurrency>ofSize(3));
-        PCollection<String> output = batchedCrypt.apply(ParDo.of(new ProcessBatch()));
-        output.apply(TextIO.write().to("output-count"));
+    // Process3: GroupIntoBatches
+    PCollection<KV<String, CryptoCurrency>> cryptoKV =
+        textData.apply(ParDo.of(new ConvertTextIntoKVCryptoCurrencyFn()));
+    PCollection<KV<String, Iterable<CryptoCurrency>>> batchedCrypt =
+        cryptoKV.apply(GroupIntoBatches.<String, CryptoCurrency>ofSize(3));
+    PCollection<String> output = batchedCrypt.apply(ParDo.of(new ProcessBatch()));
+    output.apply(TextIO.write().to("output-count"));
 
-        p.run().waitUntilFinish();
-    }
+    p.run().waitUntilFinish();
+  }
 }
