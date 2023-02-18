8d1188c8c65c84be28abd16ff2190995e1a0e50b
Masato Naka <masatonaka1989@gmail.com>
2023-02-18 10:05:41 +0900

    add ci for python

--- python/streaming_wordcount.py
+++ python/streaming_wordcount.py
@@ -25,9 +25,9 @@ import logging
 
 import apache_beam as beam
 from apache_beam.examples.wordcount_with_metrics import WordExtractingDoFn
-from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.pipeline_options import SetupOptions
-from apache_beam.options.pipeline_options import StandardOptions
+from apache_beam.options.pipeline_options import (PipelineOptions,
+                                                  SetupOptions,
+                                                  StandardOptions)
 from apache_beam.transforms import window
 
 
